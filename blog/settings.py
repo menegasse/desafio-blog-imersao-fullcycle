4176d1cc171aae78f9f7b6ba92638caa72afdc31
menegasse <menegassehugo12@gmail.com>
2024-10-30 22:50:24 -0300

    adicionando a barra ao fim da variavel  STATIC_URL

--- blog/settings.py
+++ blog/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
